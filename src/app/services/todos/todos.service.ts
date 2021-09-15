0a0b8ad8f8217d4842519bb91c91f52b1e83c61f
cheonyulin@gmail.com <cheonyulin@gmail.com>
2021-09-16 02:23:12 +0900

    todos 데이터를 컴포넌트 단이 아닌 서비스 단에서 관리할 수 있도록 상수 추가

--- src/app/services/todos/todos.service.ts
+++ src/app/services/todos/todos.service.ts
@@ -6,38 +6,43 @@ import { Todo } from './todos.model';
   providedIn: 'root',
 })
 export class TodosService {
+  /*
+    기본적으로 Todo 배열은 Service에서 관리한다. 일단, 컴포넌트를 데이터를 직접 가져오거나
+    직접저장하지 않는 것이 좋다. 컴포넌트는 오로지 데이터를 표시하는 것에만 집중하는 것이 좋다.
+  */
+  private _todos: Todo[] = TODOS;
+
   constructor() {}
 
   getTodos(): Todo[] {
-    return TODOS;
+    return this._todos;
   }
 
   addTodo(todo: Todo) {
-    const todos = TODOS;
-    todos.push(todo);
-  }
-
-  deleteTodo(id: number) {
-    const todos = TODOS;
-    const index = todos.findIndex((todo) => todo.id === id);
-    todos.splice(index, 1);
-  }
-
-  setComplete(id: number) {
-    const todos = TODOS;
-    const index = todos.findIndex((todo) => todo.id === id);
-    todos[index].isComplete = true;
-  }
-
-  setNotComplete(id: number) {
-    const todos = TODOS;
-    const index = todos.findIndex((todo) => todo.id === id);
-    todos[index].isComplete = false;
+    this._todos.push(todo);
   }
 
   editTodo(id: number, content: string) {
     const todos = TODOS;
     const index = todos.findIndex((todo) => todo.id === id);
     todos[index].content = content;
+  }
+
+  deleteTodo(id: number) {
+    this._todos = this._todos.filter((todo: Todo) => todo.id !== id);
+  }
+
+  /*
+    생성한 To do의 할 일이 모두 끝났는지 확인한다.
+  */
+  setComplete(id: number) {
+    const index = this._todos.findIndex((todo) => todo.id === id);
+    this._todos[index].isComplete = true;
+  }
+
+  setNotComplete(id: number) {
+    const todos = TODOS;
+    const index = todos.findIndex((todo) => todo.id === id);
+    todos[index].isComplete = false;
   }
 }
